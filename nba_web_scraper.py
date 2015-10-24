7ee8cb10e90cabff4f7502407d963358cab2dffa
Stefan Vujović <stefanvujovic93@gmail.com>
2015-10-24 02:09:28 +0200

    pronadjen bag sa tabelom i godinama

--- nba_web_scraper.py
+++ nba_web_scraper.py
@@ -3,62 +3,64 @@ import requests, time
 
 start = time.time()
 
-#select a 'URL patern'
-url="http://basketball.realgm.com/player/Marko-Keselj/Summary/101"
 
-#request URL
-r = requests.get(url)
-
-#read response content
-result = r.content 
-
-#make BeautifulSoup object
-soup = BeautifulSoup(result, 'html.parser')
-
-#find the table
-table = soup.find("table", {"class" : "tablesaw compact"})
-
-#find a row
-tr = table.find_all("tr", {"class" : "per_game"})
-
-table_row = tr[-1]
-
-#find table columns
-table_columns = table_row.find_all("td")
-
-div = soup.find("div", {"class":"profile-wrap"})
-
-s = div.find_all("p")
-
-service = s[1].string.strip()[0]
+f = open("C:/Users/vujke/Documents/GitHub/Scraper/NBA_Stats.txt" ,"w")
+errorFile = open("C:/Users/vujke/Documents/GitHub/Scraper/Error.txt" ,"w")
 
 
-# for x in range(21):
-#	 value = table_columns[x].string.strip()
-#   file.write(value, "\t")
+x = 100
 
-print "GP: %s" % table_columns[2].string.strip()
-print "GS: %s" % table_columns[3].string.strip()
-print "MIN: %s" % table_columns[4].string.strip()
-print "FGM: %s" % table_columns[5].string.strip()
-print "FGA: %s" % table_columns[6].string.strip()
-print "FGp: 0%s" % table_columns[7].string.strip()
-print "3FGM: %s" % table_columns[8].string.strip()
-print "3FGA: %s" % table_columns[9].string.strip()
-print "3Pp: 0%s" % table_columns[10].string.strip()
-print "FTM: %s" % table_columns[11].string.strip()
-print "FTA: %s" % table_columns[12].string.strip()
-print "FTp: 0%s" % table_columns[13].string.strip()
-print "FIC: %s" % table_columns[14].string.strip()
-print "OFF: %s" % table_columns[15].string.strip()
-print "DEF: %s" % table_columns[16].string.strip()
-print "REB: %s" % table_columns[17].string.strip()
-print "AST: %s" % table_columns[18].string.strip()
-print "STL: %s" % table_columns[19].string.strip()
-print "BLK: %s" % table_columns[20].string.strip()
-print "PF: %s" % table_columns[21].string.strip()
-print "PTS: %s" % table_columns[23].string.strip()
-print "YP: %s" % service
+while(x < 500):
+
+	#select a 'URL patern'
+	url="http://basketball.realgm.com/player/Marko-Keselj/Summary/"+str(x)
+
+	#request URL
+	r = requests.get(url)
+
+	#read response content
+	result = r.content 
+
+	#make BeautifulSoup object
+	soup = BeautifulSoup(result, 'html.parser')
+
+	#find the table
+	table = soup.find("table", {"class" : "tablesaw compact"})
+
+	#find a row
+	tr = table.find_all("tr", {"class" : "per_game"})
+
+	table_row = tr[-1]
+
+	#find table columns
+	table_columns = table_row.find_all("td")
+
+	div = soup.find("div", {"class":"profile-wrap"})
+
+	s = div.find_all("p")
+
+	#number of years spent in the NBA
+	years = s[1].string.strip()[0]
+
+	i = x-100+1
+	f.write(str(i)+"\t")
+
+	try:
+		for y in range (2, 24):
+			value = table_columns[y].string.strip()
+			f.write(str(value)+"\t")	
+
+		f.write(str(years)+"\n")
+		#f.write(str(year)+"\n")
+	
+	except Exception as e:
+            errorFile.write(str(x)+"********"+str(e)+"**********"+str(table_columns)+"\n")
+            pass
+	
+	x = x+1
+
+f.close()
+errorFile.close()
 
 print 'It took', time.time()-start, 'seconds.'
 
@@ -84,3 +86,26 @@ print 'It took', time.time()-start, 'seconds.'
 # Pitanja:
 # 1. da li mi treba broj pogodjenih ako imam procenat i broj pokusaja? 
 # 2. da li je efikasnije da ubacim for ili da imam ovako 20 linija? 
+
+# GP: 91
+# GS: 7
+# MIN: 9.5
+# FGM: 1.13
+# FGA: 2.56
+# FGp: 0.44
+# 3FGM: 0.2
+# 3FGA: 0.6
+# 3Pp: 0.30
+# FTM: 0.69
+# FTA: 1.01
+# FTp: 0.68
+# FIC: 1.75
+# OFF: 0.56
+# DEF: 1.08
+# REB: 1.64
+# AST: 0.48
+# STL: 0.29
+# BLK: 0.04
+# PF: 1.58
+# PTS: 3.16
+# YP: 2
