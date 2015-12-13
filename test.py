2f878e9db6244209da68180da3f22b217f4c9078
vujke <stefanvujovic93@gmail.com>
2015-12-13 15:22:46 +0100

    pozicije i imena su skrejpovani

--- test.py
+++ test.py
@@ -1,63 +1,90 @@
+
 from bs4 import BeautifulSoup
 import requests, time
 
-
+start = time.time()
+f = open("C:/Users/vujke/Documents/GitHub/Scraper/test.txt" ,"w")
+errorFile = open("C:/Users/vujke/Documents/GitHub/Scraper/TestError.txt" ,"w")
+x=100
+while x<500:
 	#select a 'URL patern'
-url="http://basketball.realgm.com/player/Marko-Keselj/Summary/102"
+	url="http://basketball.realgm.com/player/Marko-Keselj/Summary/"+str(x)
 
-#request URL
-r = requests.get(url)
+	#request URL
+	r = requests.get(url)
 
-if r.status_code != requests.codes.ok:
-	print "status code not good"
+	if r.status_code != requests.codes.ok:
+		x=x+1
+		continue
+		
+	try:
 
+		#read response content
+		result = r.content 
 
-#read response content
-result = r.content 
+			#make BeautifulSoup object
+		soup = BeautifulSoup(result, 'html.parser')
 
-	#make BeautifulSoup object
-soup = BeautifulSoup(result, 'html.parser')
-
-	#find the table
-
-
-	#i = x-100+1
-	#f.write(str(i)+"\t")
-
-	#f.write()
-
-name_l = soup.h2.get_text().strip()
-
-if name_l.find('#') == -1:
-
-	name_pos = name_l
-	print name_pos
-	print len(name_pos)
-	len_name_pos = len(name_l)
-
-	if(name_pos[len_name_pos-2].isalpha()):
-		position = name_pos[len_name_pos-3:]
-		name = name_pos[0:len_name_pos-4]
-	else:
-		position = name_pos[len_name_pos-1:]
-		name = name_pos[0:len_name_pos-2] #dovde bi trebalo da radi
-else:
-
-	index = name_l.index('#')
-	end = len(name_l)-index
-	name_pos = name_l[0:-(end+2)]
-	len_name_pos = len(name_pos)
-	if(name_pos[len_name_pos-2].isalpha()):
-		position = name_pos[len_name_pos-2:]
-		name = name_pos[0:len_name_pos-3]
-	elif (name_pos[len_name_pos-3].isalpha()):
-		position = name_pos[len_name_pos-3:]
-		name = name_pos[0:len_name_pos-4]
+			#find the table
 
 
-print name
+			#i = x-100+1
+			#f.write(str(i)+"\t")
 
-print position
-print len(name)
-	
+			#f.write()
+
+		name_l = soup.h2
+
+		if name_l == None:
+			x=x+1
+			continue
+
+		name_l = name_l.get_text().strip()
+
+		if name_l.find('#') == -1:
+
+			name_pos = name_l
+			#print name_pos
+			#print len(name_pos)
+			len_name_pos = len(name_l)
+			print x
+			if(name_pos[len_name_pos-2].isalpha()):
+				position = name_pos[len_name_pos-2:]
+				name = name_pos[0:len_name_pos-3]
+			else:
+				position = name_pos[len_name_pos-1:]
+				name = name_pos[0:len_name_pos-2] #dovde bi trebalo da radi
+		else:
+
+			index = name_l.index('#')
+			end = len(name_l)-index
+			name_pos = name_l[0:-(end+2)]
+			len_name_pos = len(name_pos)
+			if(name_pos[len_name_pos-2].isalpha()):
+				position = name_pos[len_name_pos-2:]
+				name = name_pos[0:len_name_pos-3]
+			elif (name_pos[len_name_pos-3].isalpha()):
+				position = name_pos[len_name_pos-1:]
+				name = name_pos[0:len_name_pos-2]
+
+		f.write(position+"\t")
+		f.write(name+"\n")
+		
+	except Exception as e:
+        	errorFile.write("Error on line: "+str(x)+"********"+str(e)+"**********"+"\n")
+        	pass
+    
+
+	x = x+1
+
+f.close()
+errorFile.close()
+
+print "done"
+time = time.time()-start
+
+if(time > 60):
+	print 'It took', time/60, 'minutes.'
+else: 
+	print 'It took', time, 'seconds.'
 	
--- test2.py
+++ test2.py
@@ -37,24 +37,33 @@ while(x < 105):
 			continue
 
 		name_l = soup.h2.get_text().strip()
-		index = name_l.index('#')
-		end = len(name_l)-index
-		name_pos = name_l[0:-(end+2)]
-		len_name_pos = len(name_pos)
-		position = ""
-		name = ""
 
-		if(name_pos[len_name_pos-2].isalpha()):
+		if name_l.find('#') == -1:
 
-			position = name_pos[len_name_pos-2:]
+			name_pos = name_l
+			#print name_pos
+			#print len(name_pos)
+			len_name_pos = len(name_l)
 
-			name = name_pos[0:len_name_pos-3]
+			if(name_pos[len_name_pos-2].isalpha()):
+				position = name_pos[len_name_pos-2:]
+				name = name_pos[0:len_name_pos-3]
+			else:
+				position = name_pos[len_name_pos-1:]
+				name = name_pos[0:len_name_pos-2] #dovde bi trebalo da radi
+		else:
 
-		elif (name_pos[len_name_pos-3].isalpha()):
+			index = name_l.index('#')
+			end = len(name_l)-index
+			name_pos = name_l[0:-(end+2)]
+			len_name_pos = len(name_pos)
+			if(name_pos[len_name_pos-2].isalpha()):
+				position = name_pos[len_name_pos-2:]
+				name = name_pos[0:len_name_pos-3]
+			elif (name_pos[len_name_pos-3].isalpha()):
+				position = name_pos[len_name_pos-1:]
+				name = name_pos[0:len_name_pos-2]
 
-			position = name_pos[len_name_pos-3:]
-
-			name = name_pos[0:len_name_pos-4]
 
 		#find a row
 		tr = table.find_all("tr", {"class" : "per_game"})
@@ -91,7 +100,12 @@ while(x < 105):
 			value = table_columns[y].string.strip()
 			f.write(str(value)+"\t")	
 
-		f.write(str(years)+"\t"+position+"\t"+name+"\n")
+		f.write(str(years))
+		print position
+		print name
+		f.write(str(position)+"\t")
+		f.write(str(name)+"\t")
+		f.write("\n")
 		#f.write(str"Err"+(year)+"\n")
 	
 	except Exception as e:
